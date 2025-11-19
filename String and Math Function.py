d0928a6896346e6c875fc5d88d26a46d55765b0e
GREG JAMES ILAO <lichtalu@gmail.com>
2025-11-19 09:18:56 +0800

    Create String and Math Function.py

---
+++ String and Math Function.py
@@ -0,0 +1,21 @@
+import math
+
+s = "  Python Programming  "
+
+print("String:", s)
+print("Length:", len(s))
+print("Lower:", s.lower())
+print("Upper:", s.upper())
+print("Strip:", s.strip())
+print("Replace:", s.replace("Python", "Java"))
+print("Split:", s.split())
+
+num = 25
+print("\nSquare Root:", math.sqrt(num))
+print("Power:", pow(3, 3))
+print("Absolute:", abs(-12))
+print("Ceil:", math.ceil(3.2))
+print("Floor:", math.floor(3.8))
+print("Factorial:", math.factorial(4))
+print("Sin 30°:", math.sin(math.radians(30)))
+print("PI:", math.pi)
